53ad4c2df9ee61c3b4c0bedfece2a9344ee72525
Komal Gill <komalgill0310@gmail.com>
2021-09-09 15:44:13 -0400

    not showing items on screen

--- script.js
+++ script.js
@@ -1,48 +1,71 @@
 document.getElementById('addExpenseToTable')
 .addEventListener('click',displayExpense);
 
-function createExpenseObject(){
-    const expenseItem = {
-    expenseType: document.getElementById('optionForPayment').value,
-    expenseDate: document.getElementById('spendingDate').value,
-    expenseName: document.getElementById('spendingName').value,
-    expenseAmount: document.getElementById('moneyUsed').value
- };
- console.log(expenseItem);
- return expenseItem;
+// function createExpenseObject(){
+//     const expenseItem = {
+//     expenseType: document.getElementById('optionForPayment').value,
+//     expenseDate: document.getElementById('spendingDate').value,
+//     expenseName: document.getElementById('spendingName').value,
+//     expenseAmount: document.getElementById('moneyUsed').value
+//  };
+//  return expenseItem;
+// }
+
+const expenseItem = {
+      expenseType: document.getElementById('optionForPayment').value,
+      expenseDate: document.getElementById('spendingDate').value,
+      expenseName: document.getElementById('spendingName').value,
+      expenseAmount: document.getElementById('moneyUsed').value
+   };
+
+function createTableRowExpenseType(){
+  const tableRowForexpenseType = document.createElement('tr');
+  return tableRowForexpenseType;
+}
+
+function createTableRowExpenseDate(){
+   const tableRowForexpenseDate = document.createElement('tr');
+   return tableRowForexpenseDate;
+}
+
+function createTableRowExpenseName(){
+  const tableRowForexpenseName = document.createElement('tr');
+  return tableRowForexpenseName;
+}
+
+function createTableRowExpenseAmount(){
+  const tableRowForexpenseAmount = document.createElement('tr');
+  return tableRowForexpenseAmount;
 }
 
 function createTableDataForTableRow(){
-  const cellLocation = document.createElement('td');
-  document.getElementById('tableRow').appendChild(cellLocation);
-  return cellLocation;
+  const tableData = document.createElement('td');
+  return tableData;
 }
-// this function does not do anything: Idea was to create textNode for each expenseItem key value. Failed
-// function displayExpense(){
-//   const cellContent = createTableDataForTableRow();
-//   // const expenseObject = createExpenseObject();
-//   const expenseItemTextNode1 = document.createTextNode
-//   (expenseItem.expenseType);
-//   const expenseItemTextNode2 = document.createTextNode
-//   (expenseItem.expenseDate);
-//   const expenseItemTextNode3 = document.createTextNode
-//   (expenseItem.expenseName);
-//   const expenseItemTextNode4 = document.createTextNode
-//   (expenseItem.expenseAmount);
-//   cellContent.appendChild(expenseItemTextNode1);
-//   cellContent.appendChild(expenseItemTextNode2);
-//   cellContent.appendChild(expenseItemTextNode3);
-//   cellContent.appendChild(expenseItemTextNode4);
-// }
 
-// this function does not give any error but it does not show the output.
 function displayExpense(){
   console.log('this functin ran');
-  const cellContent = createTableDataForTableRow();
-  const expenseObject = createExpenseObject(); 
-  const expenseItemTextNode = document.createTextNode(expenseObject);
-  cellContent.appendChild(expenseItemTextNode);
+  // const expenseObject = createExpenseObject();  
+  const tableDataInstance = createTableDataForTableRow();
+  const i = createTableRowExpenseType();
+  const j = createTableRowExpenseDate();
+  const k = createTableRowExpenseName();
+  const l = createTableRowExpenseAmount();
+  const a = tableDataInstance.appendChild(expenseItem.expenseType);
+  const b = tableDataInstance.appendChild(expenseItem.expenseDate);
+  const c = tableDataInstance.appendChild(expenseItem.expenseName);
+  const d = tableDataInstance.appendChild(expenseItem.expenseAmount);
+  const p = i.appendChild(a);
+  const q = j.appendChild(b);
+  const r = k.appendChild(c);
+  const s = l.appendChild(d);
+  document.getElementById('displayInput').appendChild(p);
+  document.getElementById('displayInput').appendChild(q);
+  document.getElementById('displayInput').appendChild(r);
+  document.getElementById('displayInput').appendChild(s);
 }
+
+
 
 
 
